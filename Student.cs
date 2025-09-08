3580159b76a8837293b2701c7766017ba0e1c38c
lamlybidash <146768077+lamlybidash@users.noreply.github.com>
2025-09-08 09:38:19 +0700

    update: model Student

--- Student.cs
+++ Student.cs
@@ -1,1 +1,19 @@
-public class
+public class Student
+{
+    public string id;
+    public string name;
+    public int age;
+    public double gpa;
+    public Student() { }
+
+    public Student(string id, string name, int age, double gpa)
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+     
+}
+
