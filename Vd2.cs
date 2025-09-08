add688e2974153ba7a0f5d92e3f6819c0089f2bc
lamlybidash <146768077+lamlybidash@users.noreply.github.com>
2025-09-08 10:05:29 +0700

    feat: AddSinhVienF

--- Vd2.cs
+++ Vd2.cs
@@ -11,18 +11,18 @@ public class BadSchoolProgram
 {
     const int QLSinhVien = 1;
     const int QLGiaoVien = 2;
-    const int QLMonHoc =3;
+    const int QLMonHoc = 3;
     const int QLDangKyHoc = 4;
-    const int QLDiem =5;
-    const int BaoCaoTongHop =6;
+    const int QLDiem = 5;
+    const int BaoCaoTongHop = 6;
     const int Thoat = 99;
 
     public static void Main(string[] args)
     {
         MainOptionF();
 
-        List<string> students = new List<string>();
-        List<string> teachers = new List<string>();
+        List<Student> students = new List<Student>();
+        List<Teachers> teachers = new List<string>();
         List<string> courses = new List<string>();
         List<string> enrollments = new List<string>();
         List<string> grades = new List<string>();
@@ -248,43 +248,116 @@ public class BadSchoolProgram
                 }
         }
     }
-    
+
+
+
+
+
+
+
+
+
+    public const int StudentAdd = 1;
+    public const int StudentDel = 2;
+    public const int StudentUpdate = 3;
+    public const int StudentAll = 4;
+    public const int StudentFilterName = 5;
+    public const int StudentFilterGPA = 6;
+    public const int StudentSortName = 7;
+    public const int StudentSortGPA = 8;
+    public const int StudentBreak = 0;
     public void QLSinhVienF()
     {
-        int smenu = 0;
-        while (smenu != 9)
+        int StudentMenu = 0;
+        while (StudentMenu != 9)
         {
             WriteLine("--- QUAN LY SINH VIEN ---");
-            WriteLine("1. Them SV");
-            WriteLine("2. Xoa SV");
-            WriteLine("3. Cap nhat SV");
-            WriteLine("4. Hien thi tat ca SV");
-            WriteLine("5. Tim SV theo ten");
-            WriteLine("6. Tim SV GPA > 8");
-            WriteLine("7. Sap xep theo ten");
-            WriteLine("8. Sap xep theo GPA");
-            WriteLine("9. Quay lai");
-            smenu = int.Parse(ReadLine());
-            SelectQLSVOption(smenu);
+            WriteLine($"{StudentAdd}. Them SV");
+            WriteLine($"{StudentDel}. Xoa SV");
+            WriteLine($"{StudentUpdate}. Cap nhat SV");
+            WriteLine($"{StudentAll}. Hien thi tat ca SV");
+            WriteLine($"{StudentFilterName}. Tim SV theo ten");
+            WriteLine($"{StudentFilterGPA}. Tim SV GPA > 8");
+            WriteLine($"{StudentSortName}. Sap xep theo ten");
+            WriteLine($"{StudentSortGPA}. Sap xep theo GPA");
+            WriteLine($"{StudentBreak}. Quay lai");
+            StudentMenu = int.Parse(ReadLine());
+            SelectQLSVOption(StudentMenu);
         }
     }
 
     public static void SelectQLSVOption(int option)
+    {
+
+        switch (option)
+        {
+            case StudentAdd:
+                {
+                    AddSinhVienF();
+                    break;
+                }
+            case StudentDel:
+                {
+                    DelSinhVienF();
+                    break;
+                }
+            case StudentUpdate:
+                {
+                    UpdateSinhVienF();
+                    break;
+                }
+            case StudentAll:
+                {
+                    AllSinhVienF();
+                    break;
+                }
+            case StudentFilterGPA:
+                {
+                    FilterGPASinhVienF();
+                    break;
+                }
+            case StudentFilterName:
+                {
+                    FilterNameSinhVienF();
+                    break;
+                }
+            case StudentSortGPA:
+                {
+                    SortGPASinhVienF();
+                    break;
+                }
+            case StudentSortName:
+                {
+                    SortNameSinhVienF();
+                    break;
+                }
+            case StudentBreak:
+                {
+                    break;
+                }
+
+
+        }
+    }
 
     public void AddSinhVienF(Student student)
     {
         Student student = new Student();
+
         Write("Nhap id: ");
-        string id = ReadLine();
-        student.Id = id;
+        student.Id = ReadLine();
+
         Write("Nhap ten: ");
-        string name = ReadLine();
+        student.name = ReadLine();
+
         Write("Nhap tuoi: ");
-        int age = int.Parse(ReadLine());
+        student.age = int.Parse(ReadLine());
+
         Write("Nhap GPA: ");
-        double gpa = double.Parse(ReadLine());
-        students.Add(id + "|" + name + "|" + age + "|" + gpa);
+        student.gpa = double.Parse(ReadLine());
 
         Students.Add(student);
     }
+    
+
 }
